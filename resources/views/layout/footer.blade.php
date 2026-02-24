b1bd1611378988ea247591c4ca01f90a68bd9bf7
Nur Diantoro <nurdiantoro100@gmail.com>
2026-02-25 06:55:02 +0700

    Add daisyUI and blade-fontawesome, update UI
    Install daisyUI (npm) and owenvoke/blade-fontawesome (composer) and update assets and views for a new UI layout. Adds daisyui to package.json/package-lock and imports the plugin + custom theme vars in resources/css/app.css; adds a mobile menu toggle script in resources/js/app.js. Update header and welcome views with a revamped navbar and mobile dropdown, and replace/add a full footer layout. Composer and package lock files updated to reflect the new dependencies.

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,6 +1,49 @@
-@if (Route::has('login'))
-    <div class="h-14.5 hidden lg:block"></div>
-@endif
+<footer class="bg-neutral-950 text-white w-full md:p-10 p-4">
+    <div
+        class="flex gap-6 md:flex-row flex-col flex-justify-between md:max-w-300 mx-auto border-b pb-4 mb-4 border-neutral-600">
+        <div class="flex flex-col">
+            <div class="w-full border-b pb-4 mb-4 border-neutral-600 font-bold text-xl">TENTANG KAMI</div>
+            <div class="text-neutral-400 text-sm">
+                <p>Telusur.co.id diterbitkan 17 Februari 2017 oleh PT Telusur Info Media, dan telah Terverifikasi
+                    Administrasi
+                    dan Faktual oleh Dewan Pers No. 302/DP-Terverifikasi/K/X/2018</p>
+                <br>
+                <p>Pada tanggal 5 Oktober 2018, juga Terdaftar di
+                    Kemenkominfo No. 00840/DJAI.PSE/04/2018.</p>
+                <br>
+                <img src="{{ asset('img/logo-telusur.webp') }}" alt="Logo Telusur">
+            </div>
+        </div>
+        <div class="flex flex-col">
+            <div class="w-full border-b pb-4 mb-4 border-neutral-600 font-bold text-xl">Tentang Kami</div>
+            <div class="text-neutral-400 text-sm">
+                <p>Telusur.co.id diterbitkan 17 Februari 2017 oleh PT Telusur Info Media, dan telah Terverifikasi
+                    Administrasi
+                    dan Faktual oleh Dewan Pers No. 302/DP-Terverifikasi/K/X/2018</p>
+                <p>Pada tanggal 5 Oktober 2018, juga Terdaftar di
+                    Kemenkominfo No. 00840/DJAI.PSE/04/2018.</p>
+            </div>
+        </div>
+        <div class="flex flex-col">
+            <div class="w-full border-b pb-4 mb-4 border-neutral-600 font-bold text-xl">Tentang Kami</div>
+            <div class="text-neutral-400 text-sm">
+                <p>Telusur.co.id diterbitkan 17 Februari 2017 oleh PT Telusur Info Media, dan telah Terverifikasi
+                    Administrasi
+                    dan Faktual oleh Dewan Pers No. 302/DP-Terverifikasi/K/X/2018</p>
+                <p>Pada tanggal 5 Oktober 2018, juga Terdaftar di
+                    Kemenkominfo No. 00840/DJAI.PSE/04/2018.</p>
+            </div>
+        </div>
+    </div>
+    <div class="flex divide-x divide-gray-400 text-xs md:max-w-300 mx-auto flex-wrap text-center md:text-left">
+        <div class="px-2 text-neutral-500">&copy; COPYRIGHT {{ date('Y') }} telusur.co.id | <a href=""
+                class="text-warna-02 hover:underline">Kebijakan Privacy</a> | <a href=""
+                class="text-warna-02 hover:underline">Pedoman Pemberitaan</a> | <a href=""
+                class="text-warna-02 hover:underline">Disclaimer</a> | <a href=""
+                class="text-warna-02 hover:underline">Tentang Kami</a> | <a href=""
+                class="text-warna-02 hover:underline">Terms Of Use</a></div>
+    </div>
+</footer>
 </body>
 
 </html>
