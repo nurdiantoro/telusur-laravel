0c4c7efbc341d04f9ef0259af08549fd3c964b01
Nur Diantoro <nurdiantoro100@gmail.com>
2026-03-27 10:22:21 +0700

    Perapihan Navbar (Part 2)
    + Navbar udah lebih clean
    + Ada suggest berita populer
    - suggest berita populer masih hardcode

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,8 +1,11 @@
-<div class="fixed top-0 z-50 flex w-full flex-col items-center md:static">
-    {{-- Logo --}}
-    <div class="flex w-full items-center justify-center border-gray-200 bg-white md:border-b" x-data="{ showSearch: false }">
+<div class="z-100 fixed top-0 flex w-full flex-col items-center md:static">
+    {{-- Navbar Mobile --}}
+    <div class="flex w-full items-center justify-center border-gray-200 bg-white md:border-b" x-data="{ showSearch: false }"
+        x-init="$watch('showSearch', val => {
+            document.body.classList.toggle('overflow-hidden', val)
+        })">
         <div
-            class="md:w-300 relative flex w-full flex-col items-center px-4 py-2 shadow-lg md:flex-row md:items-end md:justify-center md:px-0 md:py-4 md:shadow-none">
+            class="md:w-300 z-100 relative flex w-full flex-col items-center px-4 py-2 shadow-lg md:flex-row md:items-end md:justify-center md:px-0 md:py-4 md:shadow-none">
 
             {{-- Tengah / Logo --}}
             <a href="{{ route('home') }}" class="md:flex md:flex-1 md:justify-start">
@@ -11,19 +14,21 @@
             </a>
 
             {{-- Kanan --}}
-            <div class="flex w-full items-center justify-between gap-4 md:flex-1 md:justify-end">
+            <div class="mt-2 flex w-full flex-row items-center justify-between gap-4 md:flex-1 md:justify-end">
                 <div class="flex gap-4">
-                    <a href="#" class="hover:text-warna-01 font-semibold text-gray-500">Index</a>
-                    <a href="#" class="hover:text-warna-01 font-semibold text-gray-500">Opini</a>
-                    <a href="#" class="hover:text-warna-01 font-semibold text-gray-500">Berita Video</a>
+                    <a href="#" class="md:hover:text-warna-01 font-semibold text-black md:text-gray-500">Index</a>
+                    <a href="#" class="md:hover:text-warna-01 font-semibold text-black md:text-gray-500">Opini</a>
+                    <a href="#" class="md:hover:text-warna-01 font-semibold text-black md:text-gray-500">Berita
+                        Video</a>
                 </div>
 
-                <div>
+                {{-- Darkmode --}}
+                {{-- <div>
                     <button id="darkmode-toggle" type="button"
                         class="cursor-pointer rounded-lg px-3 text-gray-500 hover:bg-gray-100">
-                        <x-heroicon-c-sun />
+                        <x-heroicon-c-sun class="h-6" />
                     </button>
-                </div>
+                </div> --}}
 
                 <button type="button" @click="showSearch = !showSearch"
                     class="hover:text-warna-03 relative h-8 w-8 text-gray-500">
@@ -45,17 +50,34 @@
                 </button>
             </div>
 
-            <form method="GET" action="{{ route('search') }}" x-data x-init="$watch('showSearch', val => val && $nextTick(() => $refs.searchInput.focus()))"
-                :class="showSearch ? 'max-h-40 mt-2' : 'max-h-0'"
-                class="flex w-full flex-row gap-2 overflow-hidden transition-all duration-500 ease-in-out md:hidden">
+            {{-- Search Form Mobile --}}
+            <form method="GET" action="{{ route('search') }}" x-cloak x-data x-init="$watch('showSearch', val => val && $nextTick(() => $refs.searchInput.focus()))"
+                :class="showSearch ? 'max-h-40 mt-2 opacity-100' : 'max-h-0 opacity-0'"
+                class="flex w-full flex-row gap-2 overflow-hidden rounded-lg border border-gray-300 transition-all duration-500 ease-in-out md:hidden">
 
                 <input type="text" placeholder="Cari..." x-ref="searchInput" name="search_input" required
-                    class="w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none">
+                    class="w-full px-3 py-2 focus:outline-none">
                 <button type="submit" class="hover:text-warna-03 cursor-pointer p-2 text-gray-500">
                     <x-heroicon-c-magnifying-glass class="h-6" />
                 </button>
             </form>
+
+            <div x-cloak :class="showSearch ? 'max-h-40 mt-2 opacity-100 my-2' : 'max-h-0 opacity-0 my-0'"
+                class="flex w-full flex-col gap-1 overflow-hidden opacity-0 transition-all duration-500 ease-in-out md:hidden">
+                <span class="text-sm text-gray-500">Sering dicari</span>
+                @for ($i = 0; $i < 3; $i++)
+                    <a href="#" class="flex items-center gap-2 overflow-hidden">
+                        <x-heroicon-c-arrow-trending-up class="text-warna-03 h-6 shrink-0" />
+                        <span class="truncate text-sm text-gray-600 hover:text-black">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit
+                        </span>
+                    </a>
+                @endfor
+            </div>
         </div>
+
+        <div x-show="showSearch" x-transition.opacity @click="showSearch = false" class="-z-1 fixed inset-0 bg-black/50"
+            x-cloak></div>
     </div>
 
     {{-- Navbar Desktop --}}
